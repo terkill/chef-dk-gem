e76e98ba4333b6a359740d66f2cfac09067695dd
Franklin Hanson <fhanson@cloud-mgmt.com>
2015-10-18 23:34:33 -0400

    Cleanup style and add Makefile
    Add Rakefile for testing.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             'chef-dk-gem'
-maintainer       'Franklin Hanson'
+name 'chef-dk-gem'
+maintainer 'Franklin Hanson'
 maintainer_email 'fhanson@gannett.com'
-license          'Apache v2.0'
-description      'Installs/Deletes chef-dk user gems'
-version          '1.0.0'
+license 'Apache v2.0'
+description 'Installs/Deletes chef-dk user gems'
+version '1.0.0'
 
 depends 'chef-dk'
 
 %w(redhat centos scientific amazon fedora debian ubuntu).each do |os|
-  supports       os
+  supports os
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,9 +2,7 @@
 
 require_relative 'spec_helper'
 
-
 describe file('/root/.chefdk/gem/ruby/2.1.0/gems/knife-supermarket-0.2.1'),
          if: %w(ubuntu debian redhat fedora).include?(os[:family]) do
   it { should exist }
 end
-
